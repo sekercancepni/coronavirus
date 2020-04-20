27b1f5365e2f1393c15ab280ab4f151ff6445efe
Seker Can Cepni <sekercan.cepni@metglobal.com>
2020-04-20 15:27:06 +0300

    Changed api v2

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export const GOOGLE_API = process.env.GOOGLE_MAP_API_KEY;
 
 export const apiConfig = {
-  latestData: 'https://corona.lmao.ninja/all',
-  countries: 'https://corona.lmao.ninja/countries',
+  latestData: 'https://corona.lmao.ninja/v2/all',
+  countries: 'https://corona.lmao.ninja/v2/countries',
   historicalCountry: country => `https://corona.lmao.ninja/v2/historical/${country}`,
-  countriesSort: sortingKey => `https://corona.lmao.ninja/countries?sort=${sortingKey}`,
+  countriesSort: sortingKey => `https://corona.lmao.ninja/v2/countries?sort=${sortingKey}`,
 };
